3f0d99265e4f023774b748c86f9d2f51cea228fc
dawnhazed <shafadoesthings@gmail.com>
2025-05-24 02:02:47 +0700

    Initial Commit

---
+++ budgetingApp/budgetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetingAppApp.swift
+//  budgetingApp
+//
+//  Created by Nadaa Shafa Nadhifa on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct budgetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ budgetingAppTests/budgetingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  budgetingAppTests.swift
+//  budgetingAppTests
+//
+//  Created by Nadaa Shafa Nadhifa on 24/05/25.
+//
+
+import Testing
+@testable import budgetingApp
+
+struct budgetingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
