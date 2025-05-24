2018ee9008a3dd059b7dc36a2374a5840a72e221
dawnhazed <shafadoesthings@gmail.com>
2025-05-24 10:51:57 +0700

    Going to the buttons

---
+++ budgetingApp/Helpers/CalcButton.swift
@@ -0,0 +1,18 @@
+//
+//  CalcButton.swift
+//  budgetingApp
+//
+//  Created by Nadaa Shafa Nadhifa on 24/05/25.
+//
+
+import SwiftUI
+
+struct CalcButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalcButton()
+}
